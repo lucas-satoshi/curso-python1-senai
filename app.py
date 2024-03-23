822a12ff4250de549a5a9537e9fc2438748d80c4
Lucas Satoshi <157372920+lucas-satoshi@users.noreply.github.com>
2024-03-23 16:49:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
    return render_template('index.html')
@@ -29,27 +28,29 @@ def calculate():
     valor_b = request.form.get('valor-b')
     operador = request.form.get('operador')
 
-    if operador == "+":         
-        result = int(valor_a) + int(valor_b)
-    if operador == "-":         
-        result = int(valor_a) - int(valor_b)
-    if operador == "*":         
-        result = int(valor_a) * int(valor_b)
-    if operador == "**":         
-        result = int(valor_a) ** int(valor_b)
-    if operador == "/": 
-        if valor_b != "0":        
-            result = int(valor_a) / int(valor_b)
-        else:
-            result = "Não divisível por Zero"
-    if operador == "//":         
-        result = int(valor_a) // int(valor_b)
-    if operador == "%":         
-        result = int(valor_a) % int(valor_b)
-    if operador == "raiz":         
-        raiz = int(valor_a) ** (1/int(valor_b))
-        result = round(raiz, 2)
-
+    try:
+        if operador == "+":         
+            result = int(valor_a) + int(valor_b)
+        if operador == "-":         
+            result = int(valor_a) - int(valor_b)
+        if operador == "*":         
+            result = int(valor_a) * int(valor_b)
+        if operador == "**":         
+            result = int(valor_a) ** int(valor_b)
+        if operador == "/": 
+            try:      
+                result = int(valor_a) / int(valor_b)
+            except ZeroDivisionError as erro:
+                result = "Não divisível por Zero"
+        if operador == "//":         
+            result = int(valor_a) // int(valor_b)
+        if operador == "%":         
+            result = int(valor_a) % int(valor_b)
+        if operador == "raiz":         
+            raiz = int(valor_a) ** (1/int(valor_b))
+            result = round(raiz, 2)
+    except ValueError as erro:
+        result = "Por gentileza, insira valores válidos"
 
     return render_template('result_calc.html', result = result)
 
